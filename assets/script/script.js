9ec3da9c1a5abccf61576ec115d2872d2cea2602
nizar-rizkiana <nizar.rizkiana007@gmail.com>
2022-02-01 16:04:08 +0700

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+// var splide = new Splide( '.splide' );
+
+
+// splide.on( 'autoplay:playing', function ( rate ) {
+//   console.log( rate ); // 0-1
+// } );
+
+
+// splide.mount();
+
+// multi page center
+// var splide = new Splide( '.splide', {
+//     type   : 'loop',
+//     perPage: 3,
+//     focus  : 'center',
+//   } );
+  
+//   splide.mount();
+
+
+
+
